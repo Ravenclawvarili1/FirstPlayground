1a7f1cc670c7af5fee0fa87f580576454b49dc4f
Sqoke1ea <65782338+Sqoke1ea@users.noreply.github.com>
2022-07-24 15:10:46 +0300

    Add files via upload

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+//Variables & Constants
+
+//snake_case
+//camelCase
+
+//Variable types:
+//1. String
+var userName = "Samed"
+userName.append("909090")
+userName.lowercased()
+userName.uppercased()
+var surName = "Arslan"
+
+userName = "Merve"
+print(userName)
+
+
+//2. Integer
+
+let userAge = 50
+let myNumber = 4
+userAge / myNumber
+
+//3. Double
+
+
+let userAgeD = 50.0
+let myNumberD = 4.0
+
+userAgeD / myNumberD
+//4. boolean
+var myBoolean = false
+myBoolean = true
+
+//-----Part 2-------
+var myString : String = "50"
+var anotherNUmber : Int = 10
+var stringNumber : String = String(20)
+//Define
+
+let myVariable : String
+
+//initialization
+myVariable = "test"
+myVariable.uppercased()
+let myUpperVariable = myVariable.uppercased()
+print(myVariable, myUpperVariable)
